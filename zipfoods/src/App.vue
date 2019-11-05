f971bf5a094f40fb5083355033bfe8683e46da1b
Ping Ji <pingji@me.com>
2019-11-05 10:20:43 -0500

    zipFoods added

---
+++ zipfoods/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <img id="logo" alt="Zipfoods logo" src="./assets/images/zipfoods-logo.png">
+    <p>ZipFoods is your one-stop-shop for convenient online grocery shopping in the greater Boston area."</p>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    
+  }
+}
+</script>
+
+<style>
+@import './assets/css/zipfoods.scss';
+</style>
