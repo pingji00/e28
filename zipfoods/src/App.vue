fef7754b52241ae7f049206e4256ef33097f30c4
Ping Ji <pingji@me.com>
2019-11-07 11:07:08 -0500

    router added

--- zipfoods/src/App.vue
+++ zipfoods/src/App.vue
@@ -1,44 +1,33 @@
 <template>
   <div id="app">
     <img id="logo" alt="Zipfoods logo" src="./assets/images/zipfoods-logo.png">
-    
     <nav>
         <ul>
             <li v-for='link in links' :key='link'>
-                <a href='#' @click='page = link'>{{ link }}</a>
+                <router-link :to='paths[link]' exact>{{ link }}</router-link>
             </li>
         </ul>
     </nav>
-
-    <component :is='linkComponents[page]'></component>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import ShowProducts from './components/ShowProducts.vue';
-import ShowHome from './components/ShowHome.vue';
-import ShowCategories from './components/ShowCategories.vue';
 import { products } from './products.js';
 
 export default {
   name: 'app',
   components: {
-    ShowProducts,
-    ShowHome,
-    ShowCategories
   },
   data: function() {
       return {
-          /* Default page to start with */
-          page: 'home',
-          /* Store links in an array to maintain order */
-          links: ['home', 'products', 'categories'],
-          linkComponents: {
-              home: 'ShowHome',
-              products: 'ShowProducts',
-              categories: 'ShowCategories'
-          },
-          products: products
+        links: ['home', 'products', 'categories'],
+        paths: {
+            home: '/',
+            products: '/products',
+            categories: '/categories'
+        },
+        products: products
       }
   }
 }
--- zipfoods/src/main.js
+++ zipfoods/src/main.js
@@ -1,8 +1,26 @@
 import Vue from 'vue'
 import App from './App.vue'
+// src/main.js
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
+const routes = [
+  { path: '/', component: ShowHome },
+  { path: '/products', component: ShowProducts },
+  { path: '/categories', component: ShowCategories }
+];
+import ShowHome from './components/ShowHome.vue';
+import ShowProducts from './components/ShowProducts.vue';
+import ShowCategories from './components/ShowCategories.vue';
+
+const router = new VueRouter({
+  routes: routes,
+  mode: 'history'
+})
+
 
 Vue.config.productionTip = false
 
 new Vue({
+  router: router,
   render: h => h(App),
 }).$mount('#app')
