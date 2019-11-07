cf114b14c53db1c747685212e8cbb02de86920e6
Ping Ji <pingji@me.com>
2019-11-07 10:13:13 -0500

    diy spa done

--- zipfoods/src/App.vue
+++ zipfoods/src/App.vue
@@ -1,21 +1,49 @@
 <template>
   <div id="app">
     <img id="logo" alt="Zipfoods logo" src="./assets/images/zipfoods-logo.png">
-    <p>ZipFoods is your one-stop-shop for convenient online grocery shopping in the greater Boston area."</p>
+    
+    <nav>
+        <ul>
+            <li v-for='link in links' :key='link'>
+                <a href='#' @click='page = link'>{{ link }}</a>
+            </li>
+        </ul>
+    </nav>
+
+    <component :is='linkComponents[page]'></component>
   </div>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
+import ShowProducts from './components/ShowProducts.vue';
+import ShowHome from './components/ShowHome.vue';
+import ShowCategories from './components/ShowCategories.vue';
+import { products } from './products.js';
 
 export default {
   name: 'app',
   components: {
-    
+    ShowProducts,
+    ShowHome,
+    ShowCategories
+  },
+  data: function() {
+      return {
+          /* Default page to start with */
+          page: 'home',
+          /* Store links in an array to maintain order */
+          links: ['home', 'products', 'categories'],
+          linkComponents: {
+              home: 'ShowHome',
+              products: 'ShowProducts',
+              categories: 'ShowCategories'
+          },
+          products: products
+      }
   }
 }
 </script>
 
-<style>
-@import './assets/css/zipfoods.scss';
+<style lang="sass">
+@import './assets/css/zipfoods.scss'
 </style>
---
+++ zipfoods/src/components/ShowProduct.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="product">
+        <div class="product-name">{{ product.name }}</div>
+        <img
+            class='product-thumb'
+            :src="require('../assets/images/products/' + product.id + '.jpg')"
+            :alt='"Product image of " + product.name '
+        >
+        <p class="product-description">{{ product.description }}</p>
+        <div class="product-price">{{ product.price }}</div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ShowProduct',
+    props: ['product']
+}
+</script>
+
+<style lang="sass" scoped>
+
+</style>
